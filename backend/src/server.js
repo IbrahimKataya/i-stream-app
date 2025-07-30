154340aaab32bf7a48a793a74be1d0a5e0ea5d7a
IbrahimKataya <ikataya94@gmail.com>
2025-07-30 17:08:12 +0300

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import cors from "cors";
+import path from "path"
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -19,6 +20,8 @@ import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true, // allow the frontend to send the cookies
@@ -31,6 +34,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
